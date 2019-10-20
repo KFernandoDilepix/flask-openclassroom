147e28ec83dd91f5ad9a400260b1b8adfdd387ba
Kevin FERNANDO <kevin.fernando@supinfo.com>
2019-10-20 19:59:37 +0200

    added a control function to limit the data quantity.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from bson import json_util
 from flask import Flask, request
+from flask_cors import CORS
 
 
 def data_base(data_limit=1):
@@ -17,13 +18,30 @@ def data_base(data_limit=1):
     return data
 
 
+def control_data_quantity():
+    data_limit = request.args.get('limit')
+
+    if data_limit == None:
+        data_limit = 1
+
+    elif data_limit == 0:
+        data_limit = 1
+
+    else:
+        data_limit = int(data_limit)
+
+    data = json_util.dumps(data_base(data_limit))
+
+    return data
+
+
 def server():
     app = Flask(__name__)
+    CORS(app)
 
     @app.route('/', methods=['GET'])
     def index():
-        data_limit = int(request.args.get('limit'))
-        data = json_util.dumps(data_base(data_limit))
+        data = control_data_quantity()
 
         return data
 
