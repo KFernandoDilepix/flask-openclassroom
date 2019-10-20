dd2203b45a71eae6195b3fea09b77f991b1ffdcf
Kevin FERNANDO <kevin.fernando@supinfo.com>
2019-10-20 16:18:26 +0200

    Simple API demo

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html', title="Learning")
+from pymongo import MongoClient
+from bson import json_util
+from flask import Flask, request
 
 
-app.run(debug=True, port=3000)
+def data_base(data_limit=1):
+    client = MongoClient()
+
+    db = client['new_york']
+    restaurants_collection = db['restaurants']
+
+    data = []
+
+    for restau in restaurants_collection.find(limit=data_limit):
+        data.append(restau)
+
+    return data
+
+
+def server():
+    app = Flask(__name__)
+
+    @app.route('/', methods=['GET'])
+    def index():
+        data_limit = int(request.args.get('limit'))
+        data = json_util.dumps(data_base(data_limit))
+
+        return data
+
+    app.run(debug=True, port=3000)
+
+
+def main():
+    server()
+
+
+main()
